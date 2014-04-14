1c933b8af64726b46ee95756b65154184c761285
Dan Hansen <hansendanr+git@gmail.com>
2014-04-13 21:05:48 -0600

    Add lein profiles for Clojure dev

---
+++ home/.lein/profiles.clj
@@ -0,0 +1,11 @@
+{:user
+ {:plugins
+  [[lein-ring "0.8.10"]
+   [lein-pprint "1.1.1"]
+   [lein-kibit "0.0.8"]
+   [lein-drip "0.1.1-SNAPSHOT"]
+   [lein-ancient "0.5.5"]]
+  :dependencies
+  [[slamhound "1.5.4"]]
+  :aliases
+  {"slamhound" ["run" "-m" "slam.hound"]}}}
