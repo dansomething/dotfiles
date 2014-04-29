1cba3e80a894718b78aa18977071590ffa2a7d27
Dan Hansen <dan.hansen@readytalk.com>
2014-04-29 08:25:07 -0600

    Customize repl prompt and add a couple plugins

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,11 +1,12 @@
-{:user
- {:plugins
-  [[lein-ring "0.8.10"]
-   [lein-pprint "1.1.1"]
-   [lein-kibit "0.0.8"]
-   [lein-drip "0.1.1-SNAPSHOT"]
-   [lein-ancient "0.5.5"]]
-  :dependencies
-  [[slamhound "1.5.4"]]
-  :aliases
-  {"slamhound" ["run" "-m" "slam.hound"]}}}
+{:user {:aliases {"slamhound" ["run" "-m" "slam.hound"]}
+        :dependencies [[slamhound "1.5.4"]]
+        :plugins [[jonase/eastwood "0.1.2"]
+                  [lein-ancient "0.5.5"]
+                  [lein-difftest "2.0.0"]
+                  [lein-drip "0.1.1-SNAPSHOT"]
+                  [lein-kibit "0.0.8"]
+                  [lein-pprint "1.1.1"]
+                  [lein-ring "0.8.10"]
+                  [lein-try "0.4.1"]]
+        :search-page-size 30
+        :repl-options {:prompt (fn [ns] (str "your command, master? "))}}}
