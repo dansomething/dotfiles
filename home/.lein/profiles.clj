10a7cf47653081c1d4ef48deab5a2f4c79f1cc92
Dan Hansen <dan.hansen@readytalk.com>
2014-07-11 09:34:01 -0600

    Upgrade dependencies

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,12 +1,12 @@
 {:user {:aliases {"slamhound" ["run" "-m" "slam.hound"]}
-        :dependencies [[slamhound "1.5.4"]]
-        :plugins [[jonase/eastwood "0.1.2"]
+        :dependencies [[slamhound "1.5.5"]]
+        :plugins [[jonase/eastwood "0.1.4"]
                   [lein-ancient "0.5.5"]
                   [lein-difftest "2.0.0"]
                   [lein-drip "0.1.1-SNAPSHOT"]
                   [lein-kibit "0.0.8"]
                   [lein-pprint "1.1.1"]
-                  [lein-ring "0.8.10"]
+                  [lein-ring "0.8.11"]
                   [lein-try "0.4.1"]]
         :search-page-size 30
         :repl-options {:prompt (fn [ns] (str "[" ns "] " "your command, master? "))}}}
