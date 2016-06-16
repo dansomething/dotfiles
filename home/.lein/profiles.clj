c32d4b590c27847d2630cfda3193a25eca1df10e
Dan Hansen <hansendanr+git@gmail.com>
2016-06-16 14:31:47 -0600

    Update Clojure plugins
    Fix duplicate path entries

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,29 +1,29 @@
 {:user
  {:plugins
-  [[lein-ring "0.9.3" :exclusions [org.clojure/clojure]]
+  [[lein-ring "0.9.7" :exclusions [org.clojure/clojure]]
    [lein-difftest "2.0.0"]
-   [lein-kibit "0.0.8"]
+   [lein-kibit "0.1.2"]
    [lein-try "0.4.3"]
-   [cider/cider-nrepl "0.8.2"]
+   [cider/cider-nrepl "0.11.0"]
    [lein-drip "0.1.1-SNAPSHOT"]
    [lein-pprint "1.1.2" :exclusions [org.clojure/clojure]]
-   [lein-cljfmt "0.1.10"]
-   [codox "0.8.11" :exclusions [leinjacker]]
-   [venantius/ultra "0.3.3"]
-   [lein-kibit "0.0.8" :exclusions [org.clojure/clojure org.clojure/tools.cli org.clojure/tools.namespace]]
-   [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
-   [lein-ancient "0.6.5" :exclusions [com.fasterxml.jackson.dataformat/jackson-dataformat-smile org.clojure/clojure org.clojure/data.xml com.fasterxml.jackson.core/jackson-core]]
+   [lein-cljfmt "0.4.1"]
+   [codox "0.9.4" :exclusions [leinjacker]]
+   [venantius/ultra "0.4.1"]
+   [lein-kibit "0.1.2" :exclusions [org.clojure/clojure org.clojure/tools.cli org.clojure/tools.namespace]]
+   [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
+   [lein-ancient "0.6.8" :exclusions [com.fasterxml.jackson.dataformat/jackson-dataformat-smile org.clojure/clojure org.clojure/data.xml com.fasterxml.jackson.core/jackson-core]]
    [lein-ns-dep-graph "0.1.0-SNAPSHOT"]
    [lein-plz "0.3.3" :exclusions [rewrite-clj ancient-clj]]
-   [lein-droid "0.3.5" :exclusions [org.clojure/clojure]]
-   [lein-cooper "1.0.1" :exclusions [org.clojure/clojure]]
+   [lein-droid "0.4.3" :exclusions [org.clojure/clojure]]
+   [lein-cooper "1.2.1" :exclusions [org.clojure/clojure]]
    [lein-typed "0.3.5" :exclusions [org.clojure/clojure]]
-   [lein-shell "0.4.0" :exclusions [org.clojure/clojure]]]
+   [lein-shell "0.5.0" :exclusions [org.clojure/clojure]]]
 
   :dependencies
   [[org.clojure/tools.namespace "0.2.10"]
    [slamhound "1.5.5"]
-   [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]]
+   [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]]
 
   :ultra {:color-scheme :solarized_dark}
 
