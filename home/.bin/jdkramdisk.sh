df2ee5828cd927fc3a9a7a54f34b2d88199e525e
Dan Hansen <dan@powerproleasing.com>
2017-03-03 12:40:11 -0500

    Free up a few MB that weren't used for storage

--- home/.bin/jdkramdisk.sh
+++ home/.bin/jdkramdisk.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/env bash 
-diskutil erasevolume HFS+ 'JDKRAMDISK' `hdiutil attach -nomount ram://716800`
+#!/usr/bin/env bash
+size=$((340 * 2048))
+diskutil erasevolume HFS+ 'JDKRAMDISK' `hdiutil attach -nomount ram://$size`
 cp -r  $(/usr/libexec/java_home -v 1.8) /Volumes/JDKRAMDISK
