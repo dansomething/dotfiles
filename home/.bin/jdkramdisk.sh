09640fdb03fb949db7d4145bd0cd04b0cd6eabca
Dan Hansen <dan@powerproleasing.com>
2017-03-21 10:40:24 -0400

    Shellcheck fixes and allow running more than once

--- home/.bin/jdkramdisk.sh
+++ home/.bin/jdkramdisk.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
-size=$((340 * 2048))
-diskutil erasevolume HFS+ 'JDKRAMDISK' `hdiutil attach -nomount ram://$size`
-cp -r  $(/usr/libexec/java_home -v 1.8) /Volumes/JDKRAMDISK
+
+JDK_PATH="/Volumes/JDKRAMDISK"
+
+if [ ! -d "$JDK_PATH" ]; then
+  size=$((340 * 2048))
+  # shellcheck disable=SC2046
+  diskutil erasevolume HFS+ 'JDKRAMDISK' $(hdiutil attach -nomount ram://$size)
+  cp -r "$(/usr/libexec/java_home -v 1.8)" "$JDK_PATH"
+fi
+
+export JDK_HOME="$JDK_PATH/Home"
+unset JDK_PATH
