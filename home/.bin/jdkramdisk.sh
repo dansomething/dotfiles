e6cba235c839691eec519adb4e9d07094d264b66
Dan Hansen <dan@powerproleasing.com>
2018-11-13 08:48:32 -0600

    Give more space to Java 8

--- home/.bin/jdkramdisk.sh
+++ home/.bin/jdkramdisk.sh
@@ -3,10 +3,10 @@
 JDK_PATH="/Volumes/JDKRAMDISK"
 
 if [ ! -d "$JDK_PATH" ]; then
-  size=$((340 * 2048))
+  size=$((320 * 2048))
   # shellcheck disable=SC2046
   diskutil erasevolume HFS+ 'JDKRAMDISK' $(hdiutil attach -nomount ram://$size)
-  cp -r "$(/usr/libexec/java_home -v 1.8)" "$JDK_PATH"
+  rsync -a --exclude=*src.zip --exclude=visualvm "$(/usr/libexec/java_home -v 1.8)" "$JDK_PATH"
 fi
 
 export JDK_HOME="$JDK_PATH/Home"
