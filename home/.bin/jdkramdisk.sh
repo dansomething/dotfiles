a1b75c54918e77aeac2a569b255e4cb5655482e2
Dan Hansen <dan@powerproleasing.com>
2019-12-05 10:55:45 -0600

    Update jdkramdisk to be more generic and exclude more

--- home/.bin/jdkramdisk.sh
+++ home/.bin/jdkramdisk.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-JDK_PATH="/Volumes/JDKRAMDISK"
+VERSION=8
+MEMORY=160
+JDK_PATH="/Volumes/JDK${VERSION}RAMDISK"
 
 if [ ! -d "$JDK_PATH" ]; then
-  size=$((320 * 2048))
+  size=$((MEMORY * 2000))
+  path=$(/usr/libexec/java_home -v 1.$VERSION)
   # shellcheck disable=SC2046
-  diskutil erasevolume HFS+ 'JDKRAMDISK' $(hdiutil attach -nomount ram://$size)
-  rsync -a --exclude=*src.zip --exclude=visualvm "$(/usr/libexec/java_home -v 1.8)" "$JDK_PATH"
+  diskutil erasevolume HFS+ "JDK${VERSION}RAMDISK" $(hdiutil attach -nomount ram://$size)
+  rsync -a --exclude=*src.zip --exclude=man --exclude=visualvm "$path" "$JDK_PATH"
+  ln -s "$path"/src.zip "$JDK_PATH"/Home
 fi
 
-export JDK_HOME="$JDK_PATH/Home"
+export JAVA_HOME="$JDK_PATH/Home"
 unset JDK_PATH
