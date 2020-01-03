4e13b091d3bf3a17f862070b553f46a690a47389
Dan Hansen <dan@powerproleasing.com>
2020-01-03 07:57:16 -0600

    Replace Spectacle and Karabiner-Elements with Hammerspoon

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -1,40 +1,9 @@
--- First, be sure to install hs-knu.
--- git clone https://github.com/knu/hs-knu.git ~/.hammerspoon/knu
--- TODO Take a look at https://github.com/jasonrudolph/ControlEscape.spoon
--- it will replace the knu config
--- Then install miro-windows-manager
+-- Install these to ~/.hammerspoon/Spoons
+-- https://github.com/jasonrudolph/ControlEscape.spoon
 -- https://github.com/miromannino/miro-windows-manager
 
--- hs-knu modules
-knu = require("knu")
--- Function to guard a given object from GC
-guard = knu.runtime.guard
-
--- Enable auto-restart when any of the *.lua files under ~/.hammerspoon/ is modified
-knu.runtime.autorestart(true)
-
--- Switch between Karabiner-Elements profiles by keyboard
-function switchKarabinerElementsProfile(name)
-  hs.execute(knu.utils.shelljoin(
-      "/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli",
-      "--select-profile",
-      name
-  ))
-  hs.execute("pkill karabiner_console_user_server")
-end
-
-knu.usb.onChange(function (device)
-    local name = device.productName
-    if name and name:find("Keyboard") then
-      if device.eventType == "added" then
-        hs.alert.show("Filco Keyboard Enabled")
-        switchKarabinerElementsProfile("filco")
-      else
-        hs.alert.show("Default Keyboard Enabled")
-        switchKarabinerElementsProfile("default")
-      end
-    end
-end)
+-- ControlEscape.spoon
+hs.loadSpoon('ControlEscape'):start()
 
 -- Miro's windows manager
 local hyper = {"alt", "cmd"}
