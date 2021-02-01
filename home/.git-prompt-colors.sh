980fc86b0601848034d604b8a5ddb3c42655fe84
Dan Hansen <dan@powerproleasing.com>
2021-02-01 09:07:44 -0600

    Add new options from git-prompt default template

--- home/.git-prompt-colors.sh
+++ home/.git-prompt-colors.sh
@@ -33,6 +33,7 @@ override_git_prompt_colors() {
   # GIT_PROMPT_SEPARATOR="|"              # separates each item
 
   # GIT_PROMPT_BRANCH="${Magenta}"        # the git branch that is active in the current directory
+  # GIT_PROMPT_MASTER_BRANCH="${GIT_PROMPT_BRANCH}" # used if the git branch that is active in the current directory is $GIT_PROMPT_MASTER_BRANCHES
   # GIT_PROMPT_STAGED="${Red}●"           # the number of staged files/directories
   # GIT_PROMPT_CONFLICTS="${Red}✖ "       # the number of files in conflict
   # GIT_PROMPT_CHANGED="${Blue}✚ "        # the number of changed files
@@ -56,6 +57,11 @@ override_git_prompt_colors() {
   ## the name of the current virtual environment (currently CONDA and VIRTUAL_ENV)
   # GIT_PROMPT_VIRTUALENV="(${Blue}_VIRTUALENV_${ResetColor}) "
 
+  # template for displaying the current remote tracking branch
+  # use the placeholder _UPSTREAM_ will be replaced with
+  # the name of the current remote tracking branch
+  # GIT_PROMPT_UPSTREAM=" {${Blue}_UPSTREAM_${ResetColor}}"
+
   ## _LAST_COMMAND_INDICATOR_ will be replaced by the appropriate GIT_PROMPT_COMMAND_OK OR GIT_PROMPT_COMMAND_FAIL
   # GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_ ${Yellow}${PathShort}${ResetColor}"
   GIT_PROMPT_START_USER="$(get_username)$(get_hostname)${Yellow}${PathShort}${ResetColor}"
@@ -71,6 +77,12 @@ override_git_prompt_colors() {
   # GIT_PROMPT_SYMBOLS_BEHIND="↓·"            # The symbol for "n versions behind of origin"
   # GIT_PROMPT_SYMBOLS_PREHASH=":"            # Written before hash of commit, if no name could be found
   # GIT_PROMPT_SYMBOLS_NO_REMOTE_TRACKING="L" # This symbol is written after the branch, if the branch is not tracked
+
+  # branch name(s) that will use $GIT_PROMPT_MASTER_BRANCH color
+  # To specify multiple branches, use
+  #   shopt -s extglob
+  #   GIT_PROMPT_MASTER_BRANCHES='@(master|production)'
+  # GIT_PROMPT_MASTER_BRANCHES="master"
 }
 
 reload_git_prompt_colors "Custom"
